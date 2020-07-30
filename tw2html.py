6723eda77210208d41084e2f66da498b195ef63c
d34db33f-1007 <63560479+d34db33f-1007@users.noreply.github.com>
2020-07-30 11:44:10 +0300

    Update tw2html.py

--- tw2html.py
+++ tw2html.py
@@ -6,23 +6,18 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
-#from selenium.common.exceptions import StaleElementReferenceException
-#from selenium.common.exceptions import NoSuchElementException
-#from selenium.webdriver.support import expected_conditions
-#from selenium.webdriver.support.ui import WebDriverWait
-#from selenium.webdriver.common.by import By
-
 # binary = input('Full path to Chromium binary: ')
 
 tw = input('Enter Twonky address in <ip:port> format: ')
 
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-logging')
+options.add_argument('--ignore-certificate-errors')
+# options.binary_location = binary
+
 try:
-	options = Options()
-	options.add_argument('--headless')
-	options.add_argument('--no-sandbox')
-	options.add_argument('--disable-logging')
-	options.add_argument('--ignore-certificate-errors')
-	# options.binary_location = binary
 	driver = webdriver.Chrome(options=options)
 	driver.set_page_load_timeout(75)
 	driver.get(f'http://{tw}/webbrowse#photo')
@@ -42,10 +37,7 @@ r_n = 0
 d = {}
 
 
-
-
 def get_pages(p):
-
 	try:
 		pages = driver.find_elements_by_xpath(f'//*[@id=\
 "browsePages"]/div[1]/a[{p}]')[0]
@@ -55,14 +47,8 @@ def get_pages(p):
 		pass
 
 
-
 def get_albums():
-	global alt_version
-	global albums
-	global a_num
-	global r_n
-	global d
-
+	global alt_version, albums, a_num, r_n, d
 
 	if r_n == 0:
 		try:
@@ -74,9 +60,10 @@ def get_albums():
 			driver.quit()
 			exit(0)
 
-
 		a_tag = 0
 		for tit in title:
+			if a_tag > 10:
+				break
 			try:
 				al_num = tit.find_elements_by_class_name('smallFont')
 				a_name = tit.find_elements_by_class_name('truncate')
@@ -96,10 +83,8 @@ version of Twonky firmware, sorry :c')
 					driver.quit()
 					exit(0)
 
-
 	p = 1
 	while True:
-
 		try:
 			title = driver.find_elements_by_id('title')
 			a_sum = len(title)
@@ -107,8 +92,6 @@ version of Twonky firmware, sorry :c')
 			break
 
 		if a_sum < 1:
-#			if album:
-#				old_albums.append(album)
 			print(f'Not a recursive album..skip \
 [recursion: {r_n} / 10]', end='\r')
 			break
@@ -127,23 +110,16 @@ Total: {a_num}.    [page: {p} | recursion: {r_n} / 10]', end='\r')
 				find = driver.find_elements_by_class_name('byFolderContainer')
 				if alt_version:
 					f_inner = find[a].get_attribute('innerHTML')
-					src = f_inner.split("'")[1]
 				else:
-					f_click = find[a].get_attribute('onclick')
-					src = f_click.split("'")[1]
-
+					f_inner = find[a].get_attribute('onclick')
+				src = f_inner.split("'")[1]
 				albm = f'http://{tw}/webbrowse#{src}'
-
 				if albm not in albums:
 					albums.append(albm)
 					a_num += 1
-#				else:
-#					if album:
-#						old_albums.append(albm) #list(set().union(albums, old_albums))
 			except:
 				#traceback.print_exc()
 				continue
-
 
 		try:
 			find_p = driver.find_elements_by_xpath('//*[@id=\
@@ -160,9 +136,7 @@ Total: {a_num}.    [page: {p} | recursion: {r_n} / 10]', end='\r')
 		else:
 			break
 
-
 def recursion():
-
 	for album in albums:
 		try:
 			driver.get(album)
@@ -170,17 +144,11 @@ def recursion():
 		except:
 			continue
 
-
-
-def get_links(album, counter):
-	global alt_version
-	global albums
-	global links
+def get_links(album, counter, succ=True):
+	global alt_version, albums, links
 
 	print(f'\n\nTrying to scrap pics from album\
  {counter} / {len(albums)}..')
-
-	succ = True
 	try:
 		driver.get(album)
 		time.sleep(2)
@@ -191,7 +159,6 @@ def get_links(album, counter):
 
 	p = 1
 	while succ:
-
 		try:
 			photos = driver.find_elements_by_class_name('allPhotosItem')
 		except:
@@ -203,14 +170,10 @@ def get_links(album, counter):
 			print('Oops..no pics here..')
 			break
 
-#		ignored_exceptions = (NoSuchElementException, StaleElementReferenceException)
-
 		d_div = 0
 		for pic in photos:
-
 #			if d_div % 5 == 0:
 #				time.sleep(1)
-
 			if d_div == 10 or d_div % 100 == 0:
 				html = driver.find_element_by_tag_name('html')
 				for f in range(60):
@@ -224,7 +187,6 @@ def get_links(album, counter):
 
 			if alt_version:
 				try:
-#					url = WebDriverWait(driver, 20, ignored_exceptions = ignored_exceptions).until(expected_conditions.presence_of_element_located((By.XPATH, f'//*[@id="#item{d_div}"]/div/div[1]/a')))
 					url = pic.find_elements_by_xpath(f'//*[@id=\
 "item{d_div}"]/div/div[1]/a')
 					r_url = url[0].get_attribute('onclick')
@@ -239,9 +201,8 @@ def get_links(album, counter):
 
 			else:
 				try:
-					o_div = d_div + 1
 					url = pic.find_elements_by_xpath(f'//*[@id="browseContents"]/\
-div[3]/div/div[{o_div}]/div[1]/a')
+div[3]/div/div[{d_div+1}]/div[1]/a')
 					r_url = url[0].get_attribute('href')
 					b_ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', r_url)
 				except:
@@ -251,7 +212,6 @@ div[3]/div/div[{o_div}]/div[1]/a')
 					d_div += 1
 					continue
 				d_url = r_url.replace(b_ip[0], tw.split(':')[0])
-
 
 			if d_url and d_url not in links:
 				links.append(d_url)
@@ -285,23 +245,18 @@ def html():
 		for link in links:
 			try:
 				img = '<img height="176" width="320" src="%s " \
-	onerror="this.style.display=\'none\'" ' % (link)
+onerror="this.style.display=\'none\'" ' % (link)
 				html.write(img + 'onclick="window.\
-	open(this.src)">' + '\n')
+open(this.src)">' + '\n')
 				url += 1
-				print(f'[{url} / {len(links)}] Writing album\
-	 pictures to .html file..', end='\r')
+				print(f'[{url} / {len(links)}] Writing album \
+pictures to .html file..', end='\r')
 			except:
 				#traceback.print_exc()
 				continue
 
-
 def main():
-	global albums
-	global a_num
-	global r_n
-	global d
-
+	global albums, a_num, r_n, d
 
 	print('Starting to \
 scrap albums..')
@@ -314,25 +269,18 @@ Twonky server or dead. Check.')
 #		driver.quit()
 #		exit(0)
 
-	n = 0
 	print('\n\n')
 	for albm, total in d.items():
 		print(f'Found album: {albm} :: {total} photos')
-		n += 1
 	print(f'\n\nTotal found {a_num} albums')
-
-
 	print('\nTrying recursive scoping..\n')
 
 	while r_n < 10:
-
 		r_n += 1
 		recursion()
 
-
 	albums.append(f'http://{tw}\
 /webbrowse#photo')
-
 
 	counter = 0
 	for album in albums:
@@ -344,18 +292,10 @@ Twonky server or dead. Check.')
 			time.sleep(1)
 			continue
 
-
 	html()
 	print('\n')
-
 	driver.quit()
 	exit(0)
 
-
 if __name__ == "__main__":
-    main()
-
-
-
-
-
+	main()
